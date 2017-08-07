b7d388df9eb7242c7a3e19e17c7b667063239108
wilkenshuang <wilkenshuang@gmail.com>
2017-08-07 16:52:33 +0800

    Update Two_sum.py

--- Two_sum.py
+++ Two_sum.py
@@ -15,20 +15,3 @@ class solution:
                 return [buff_dict[num[i]],i]
             else:
                 buff_dict[target-num[i]]=i
-                
-class Solution:
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        if len(nums)<=1:
-            return False
-        buff_dict={}
-        for i in range(len(nums)):
-            buff_dict[target-nums[i]]=i
-            if nums[i] in buff_dict:
-                return [buff_dict[nums[i]],i]
-            else:
-                continue
